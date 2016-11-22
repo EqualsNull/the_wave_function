5d2f8f1e28bbfc873deac39d750b77bd3f2ef6f3
eqsnull <jorgecastilloltsd@gmail.com>
2016-11-22 01:54:54 -0500

    messing around with color

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+## author: Jorge Castillo
+
+import numpy as np
 from vispy import app, gloo
+from vispy.gloo.util import _screenshot
 
 vertex = """
 attribute vec2 a_position;
@@ -17,7 +21,7 @@ uniform float u_time;
 varying vec2 v_position;
 
 /**********************************************************
-Specify the parameters here.
+        parameters
 **********************************************************/
 const float z_offset = 1.5;  // (z+z_offset)/z_max should be in [0,1]
 const float z_max = 2.;
@@ -34,43 +38,43 @@ float f(float x, float y, float t) {
     // t is in [0, +oo)
 
     /**********************************************************
-    Write your function below.
+    FUNCTIONS.
     **********************************************************/
 
-    float k = pow(cos(t*490), 2)/(M_PI);
+    float k = pow(cos(t*10), 2)/(21);
 
-    float i1 = cos(t/2)*0 + 1;
-    float j1 = sin(t/2)*0;
+    float transition = (10/pow(t, 1.5));
 
-    float i2 = -cos(t/2)*0 - 1;
-    float j2 = -sin(t/2)*0;
+    float i1 = cos(t/4)*0 + transition*0;
+    float j1 = sin(t/4)*0;
 
-    return ( ( ( pow(x-i1, 2) + pow(y-j1, 2) )/1.5
-            *  (pow(x-i2, 2) + pow(y-j2, 2))/1.5
-            + 0)/2);
+    float i2 = -cos(t/4)*0 - transition*0;
+    float j2 = -sin(t/4)*0;
+
+    return (((pow(x-1.5, 2) + pow(y-0, 2) )/1.5
+            *(pow(x+1.5, 2) + pow(y-0, 2))/1.5
+            + k)/2);
 
     /*********************************************************/
 
 }
 
 vec4 jet(float x) {
+
     vec3 a, b;
     float c;
-    if (x < 0.34) {
-        a = vec3(0, 0, 0.5);
-        b = vec3(0, 0.8, 0.95);
-        c = (x - 0.0) / (0.34 - 0.0);
-    } else if (x < 0.64) {
-        a = vec3(0, 0.8, 0.95);
-        b = vec3(0.85, 1, 0.04);
-        c = (x - 0.34) / (0.64 - 0.34);
-    } else if (x < 0.89) {
-        a = vec3(0.85, 1, 0.04);
-        b = vec3(0.96, 0.7, 0);
-        c = (x - 0.64) / (0.89 - 0.64);
+
+    // rgb(132,255,255)
+    // rgb(0,188,212)
+    // rgb(66,66,66)
+
+    if (x < 0.89) {
+         a = vec3(0, 188./255., 212./255.);
+         b = vec3(132/255., 188./255., 212./255.);
+         c = (x - 0.64) / (0.89 - 0.64);
     } else {
-        a = vec3(0.96, 0.7, 0);
-        b = vec3(0.5, 0, 0);
+        a = vec3(132./255., 188./255., 212./255.);
+        b = vec3(1./255., 1./255., 33./255.);
         c = (x - 0.89) / (1.0 - 0.89);
     }
     return vec4(mix(a, b, c), 1.0);
@@ -79,7 +83,7 @@ vec4 jet(float x) {
 void main() {
     vec2 pos = v_position;
     float z = f(x_scale * pos.x, y_scale * pos.y, t_scale * u_time);
-    gl_FragColor = (jet((z + z_offset) / (z_max))*2);
+    gl_FragColor = (jet((z + z_offset) / (z_max))*1.5);
 }
 """
 
@@ -109,6 +113,16 @@ class Canvas(app.Canvas):
     def on_draw(self, event):
         self.program.draw('triangle_strip')
 
+    def movie_animate(self, t):
+        self.program['u_time'] = t
+        self.update()
+        gloo.clear()
+        self.program.draw('triangle_strip')
+        return _screenshot((0, 0, self.size[0], self.size[1]))[:,:,:3]
+
 if __name__ == '__main__':
+    #from moviepy.editor import VideoClip
     canvas = Canvas()
+    #clip = VideoClip(canvas.movie_animate, duration=10)
+    #clip.write_gif('hyrdogen.gif', fps=20, opt='OptimizePlus')
     app.run()
